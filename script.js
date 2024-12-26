7074a90f3d9c24242ae457b5f6693f76515a4c4d
martindwwm <kirizvki0@gmail.com>
2024-12-26 12:44:27 +0100

    Add auto play feature

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// @ts-ignore
 let score = JSON.parse(localStorage.getItem("score")) || {
   wins: 0,
   losses: 0,
   ties: 0,
 };
 
-// Score
 updateScore();
 
 // Button rock paper scissors
@@ -13,23 +11,20 @@ const btnRock = document.getElementById("js-btn-rock");
 const btnPaper = document.getElementById("js-btn-paper");
 const btnScissors = document.getElementById("js-btn-scissors");
 const btnResetScore = document.getElementById("js-reset-score");
+const btnAutoPlay = document.querySelector(".js-auto-play-button");
 
-// Button rock
 btnRock?.addEventListener("click", () => {
   playGames("rock");
 });
 
-// Button paper
 btnPaper?.addEventListener("click", () => {
   playGames("paper");
 });
 
-// Button scissors
 btnScissors?.addEventListener("click", () => {
   playGames("scissors");
 });
 
-// Button reset score
 btnResetScore?.addEventListener("click", () => {
   score.wins = 0;
   score.losses = 0;
@@ -38,9 +33,37 @@ btnResetScore?.addEventListener("click", () => {
   updateScore();
 });
 
+btnAutoPlay?.addEventListener("click", () => {
+  autoPlay();
+});
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(function () {
+      const playerMove = pickComputerMove();
+      playGames(playerMove);
+    }, 1000);
+
+    if (btnAutoPlay) {
+      btnAutoPlay.innerHTML = "Stop";
+    }
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+
+    if (btnAutoPlay) {
+      btnAutoPlay.innerHTML = "Auto Play";
+    }
+    isAutoPlaying = false;
+  }
+}
+
 // Play games
 function playGames(playerMove) {
-  const computerMove = pickedComputerMove();
+  const computerMove = pickComputerMove();
   let result = "";
   // Rock
   if (playerMove === "rock") {
@@ -83,16 +106,14 @@ function playGames(playerMove) {
   // Storage Score
   localStorage.setItem("score", JSON.stringify(score));
 
-  // display score
   updateScore();
 
-  // Display result
+  // result
   const displayResult = document.querySelector(".js-result");
   if (displayResult) {
     displayResult.innerHTML = `${result}`;
   }
 
-  // display player move & computer move
   const displayMoves = document.querySelector(".js-moves");
   if (displayMoves) {
     displayMoves.innerHTML = `You
@@ -110,8 +131,7 @@ function playGames(playerMove) {
   }
 }
 
-// Computer generated movement
-function pickedComputerMove() {
+function pickComputerMove() {
   const randomNumber = Math.random();
   let computerMove = "";
 
@@ -125,7 +145,6 @@ function pickedComputerMove() {
   return computerMove;
 }
 
-// Display score
 function updateScore() {
   const displayScore = document.querySelector(".js-score");
   if (displayScore) {
